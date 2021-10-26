ef0dc6f051909a2d0682f635b2382e4885dacda0
Muhammad234 <60900119028@uin-alauddin.ac.id>
2021-10-26 12:19:37 +0800

    selamat pagi

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,34 @@
+<html>
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1 align="center">BIODATA DIRI</h1>
+        <tr align="center" bgcolor=white>
+            <td width="200">DATA DIRI</td>
+            <td width="300">KETERANGAN</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>Muhammad Azrial Mahesha</td>
+        </tr>
+        <tr>
+            <td>Kelas</td>
+            <td>B</td>
+        </tr>
+        <tr>
+            <td>NIM</td>
+            <td>60900119028</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>60900119028@uin-alauddin.ac.id</td>
+        </tr>
+        
+    </table>
+</body>
+
+</html>
+
+</html>
+
